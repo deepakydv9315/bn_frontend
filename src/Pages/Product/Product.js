226cb6bda9fda2cc911a244877e2ae6eb959944d
sparrow-code <shivamdwivedi024@gmail.com>
2023-11-05 10:36:01 +0530

    preRelease

--- src/Pages/Product/Product.js
+++ src/Pages/Product/Product.js
@@ -1,123 +1,149 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from "react";
 import "./Product.scss";
 import Sidebar from "../../Components/Filter/Filter";
 import SearchBar from "../../Components/SearchBar/SearchBar";
-import ProductList from '../../Components/ProductList/ProductList';
+import ProductList from "../../Components/ProductList/ProductList";
+
+import { getAllProducts } from "../../src/Redux/slices/productSlice";
+import { useDispatch } from "react-redux";
 
 export default function Product() {
-    const filters = [
-        {
-            category: 'Category',
-            options: ['Mass gainer', 'Protein'],
-        },
-        {
-            category: 'Price',
-            options: ['Low to High', 'High to Low'],
-        },
-        {
-            category: 'Flavour',
-            options: ['Chocolate', 'Vanilla', 'Unflavoured'],
-        },
-    ];
+  const dispatch = useDispatch();
+  const filters = [
+    {
+      category: "Category",
+      options: ["Mass gainer", "Protein"],
+    },
+    {
+      category: "Price",
+      options: ["Low to High", "High to Low"],
+    },
+    {
+      category: "Flavour",
+      options: ["Chocolate", "Vanilla", "Unflavoured"],
+    },
+  ];
 
-    const products = [
-        {
-            id: 1,
-            name: 'Product 1',
-            title: 'Description for Product 1',
-            price: "999",
-            image: require('../../Assets/Images/main1.png'),
-        },
-        {
-            id: 2,
-            name: 'Product 2',
-            title: 'Description for Product 2',
-            price: "999",
-            image: require('../../Assets/Images/main2.png'),
-        },
-        {
-            id: 3,
-            name: 'Product 3',
-            title: 'Description for Product 3',
-            price: "999",
-            image: require('../../Assets/Images/main1.png'),
-        },
-        {
-            id: 4,
-            name: 'Product 4',
-            title: 'Description for Product 4',
-            price: "999",
-            image: require('../../Assets/Images/main2.png'),
-        },
-        {
-            id: 5,
-            name: 'Product 5',
-            title: 'Description for Product 5',
-            price: "999",
-            image: require('../../Assets/Images/main1.png'),
-        },
-        {
-            id: 6,
-            name: 'Product 6',
-            title: 'Description for Product 6',
-            price: "999",
-            image: require('../../Assets/Images/main2.png'),
-        },
-        // Add more products as needed
-    ];
+  const [products, setProducts] = React.useState([
+    {
+      id: 1,
+      name: "Product 1",
+      title: "Description for Product 1",
+      price: "199",
+      image: require("../../Assets/Images/main1.png"),
+    },
+    {
+      id: 2,
+      name: "Product 2",
+      title: "Description for Product 2",
+      price: "299",
+      image: require("../../Assets/Images/main2.png"),
+    },
+    {
+      id: 3,
+      name: "Product 3",
+      title: "Description for Product 3",
+      price: "399",
+      image: require("../../Assets/Images/main1.png"),
+    },
+    {
+      id: 4,
+      name: "Product 4",
+      title: "Description for Product 4",
+      price: "499",
+      image: require("../../Assets/Images/main2.png"),
+    },
+    {
+      id: 5,
+      name: "Product 5",
+      title: "Description for Product 5",
+      price: "599",
+      image: require("../../Assets/Images/main1.png"),
+    },
+    {
+      id: 6,
+      name: "Product 6",
+      title: "Description for Product 6",
+      price: "699",
+      image: require("../../Assets/Images/main2.png"),
+    },
+    // Add more products as needed
+  ]);
 
-    const [selectedFilters, setSelectedFilters] = useState([]);
-    const [sortBy, setSortBy] = useState(null);
-    const [searchQuery, setSearchQuery] = useState('');
+  useEffect(() => {
+    if (categoryname) {
+      setActiveFilter(categoryname);
+      dispatch(getAllProducts({ category: categoryname }));
+    } else {
+      dispatch(getAllProducts());
+    }
 
-    const handleFilterChange = (category, option) => {
-        const updatedFilters = selectedFilters.includes(option)
-            ? selectedFilters.filter((filter) => filter !== option)
-            : [...selectedFilters, option];
-        setSelectedFilters(updatedFilters);
-    };
+    dispatch(getAllCategories());
+  }, [dispatch, categoryname]);
 
-    const handleSortChange = (value) => {
-        setSortBy(value);
-    };
+  const [selectedFilters, setSelectedFilters] = useState([]);
+  const [sortBy, setSortBy] = useState(null);
+  const [searchQuery, setSearchQuery] = useState("");
 
-    const filteredProducts = products.filter((product) => {
-        // Apply filters and search query here
-        return (
-            (selectedFilters.length === 0 || selectedFilters.includes(product.size)) &&
-            (searchQuery === '' || product.name.toLowerCase().includes(searchQuery.toLowerCase()))
-        );
-    });
+  const handleFilterChange = (category, option) => {
+    const updatedFilters = selectedFilters.includes(option)
+      ? selectedFilters.filter((filter) => filter !== option)
+      : [...selectedFilters, option];
+    setSelectedFilters(updatedFilters);
+  };
 
+  const handleSortChange = (value) => {
+    setSortBy(value);
+  };
+
+  const filteredProducts = products.filter((product) => {
+    // Apply filters and search query here
     return (
-        <div>
-            <section className='contain'>
-                <div className='title1 product'>
-                    Products
-                </div>
-                <div className="app">
-                    <Sidebar filters={filters} selectedFilters={selectedFilters} handleFilterChange={handleFilterChange} />
-                    <div className="main-content">
-                        <div className="top-bar">
-                            <div className="search-bar">
-                                <SearchBar searchQuery={searchQuery} setSearchQuery={setSearchQuery} />
-                            </div>
-                        </div>
-                        <div className='filters'>
-                            <div className="product-count">{filteredProducts.length} Products</div>
-                            <div className="sort-by">
-                                <select value={sortBy} onChange={(e) => handleSortChange(e.target.value)}>
-                                    <option value="">Sort By</option>
-                                    <option value="lowToHigh">Price: Low to High</option>
-                                    <option value="highToLow">Price: High to Low</option>
-                                </select>
-                            </div>
-                        </div>
-                        <ProductList products={filteredProducts} />
-                    </div>
-                </div>
-            </section>
-           
+      (selectedFilters.length === 0 ||
+        selectedFilters.includes(product.size)) &&
+      (searchQuery === "" ||
+        product.name.toLowerCase().includes(searchQuery.toLowerCase()))
+    );
+  });
+
+  return (
+    <div>
+      <section className="contain">
+        <div className="title1 product">Products</div>
+        <div className="app">
+          <Sidebar
+            filters={filters}
+            selectedFilters={selectedFilters}
+            handleFilterChange={handleFilterChange}
+          />
+          <div className="main-content">
+            <div className="top-bar">
+              <div className="search-bar">
+                <SearchBar
+                  searchQuery={searchQuery}
+                  setSearchQuery={setSearchQuery}
+                />
+              </div>
+            </div>
+            <div className="filters">
+              <div className="product-count">
+                {filteredProducts.length} Products
+              </div>
+              <div className="sort-by">
+                <select
+                  value={sortBy}
+                  onChange={(e) => handleSortChange(e.target.value)}
+                >
+                  <option value="">Sort By</option>
+                  <option value="lowToHigh">Price: Low to High</option>
+                  <option value="highToLow">Price: High to Low</option>
+                </select>
+              </div>
+            </div>
+            <ProductList products={filteredProducts} />
+          </div>
         </div>
-    )
+      </section>
+    </div>
+  );
 }
--- src/utils/axios/axios.js
+++ src/utils/axios/axios.js
@@ -4,16 +4,16 @@ import axios from "axios";
 let baseURL = "http://localhost:4000";
 
 export const axiosClient = axios.create({
-    baseURL,
-    withCredentials: true,
+  baseURL,
+  withCredentials: true,
 });
 
 const httpClient = axios.create({
-    baseURL: `${process.env.REACT_APP_CONVERTKIT_API_URL}`,
-  });
-  
-  httpClient.defaults.headers.common['Accept'] = 'application/json';
-  httpClient.defaults.headers.common['Content-Type'] = 'application/json';
-  httpClient.defaults.timeout = 5000;
-  
-  export { httpClient };
+  baseURL: `${process.env.REACT_APP_CONVERTKIT_API_URL}`,
+});
+
+httpClient.defaults.headers.common["Accept"] = "application/json";
+httpClient.defaults.headers.common["Content-Type"] = "application/json";
+httpClient.defaults.timeout = 5000;
+
+export { httpClient };
